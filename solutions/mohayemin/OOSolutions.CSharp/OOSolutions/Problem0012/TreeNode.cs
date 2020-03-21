c61bbf4726fadd72db46294e777ecda665e1e6fe
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-03-21 15:36:36 +0600

    solution for #12

---
+++ solutions/mohayemin/OOSolutions.CSharp/OOSolutions/Problem0012/TreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OOSolutions.Problem0012
+{
+    class TreeNode
+    {
+        private readonly double? value;
+        private readonly List<TreeNode> children;
+        private TreeNode parent;
+
+        public TreeNode(double? value = null)
+        {
+            this.value = value;
+            children = new List<TreeNode>();
+        }
+
+        public double? GetValue()
+        {
+            if (children.Count != 0)
+                return children.Sum(child => child.GetValue());
+            else
+                return value;
+        }
+
+        public TreeNode AddChild(TreeNode child)
+        {
+            if (child.parent != null)
+                child.parent.children.Remove(child);
+
+            child.parent = this;
+            children.Add(child);
+
+            return this;
+        }
+    }
+}
---
+++ solutions/mohayemin/OOSolutions.CSharp/OOSolutions/Problem0012/TreeNodeTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace OOSolutions.Problem0012
+{
+    public class TreeNodeTest
+    {
+        [Fact]
+        void Leaf()
+        {
+            var node = new TreeNode(12);
+            Assert.Equal(12, node.GetValue());
+        }
+
+        [Fact]
+        void NonLeaf()
+        {
+            var node1 = new TreeNode(12);
+            var node2 = new TreeNode(13);
+            var node3 = new TreeNode();
+            node3
+                .AddChild(node1)
+                .AddChild(node2);
+
+            Assert.Equal(25, node3.GetValue());
+        }
+    }
+}
