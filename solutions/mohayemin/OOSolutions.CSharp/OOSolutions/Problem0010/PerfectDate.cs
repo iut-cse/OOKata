913fd9b3bbedaac8b5be29108b94ef1348ba879c
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-03-29 16:42:44 +0600

    solve #10. A perfect day

---
+++ solutions/mohayemin/OOSolutions.CSharp/OOSolutions/Problem0010/PerfectDate.cs
@@ -0,0 +1,35 @@
+﻿namespace OOSolutions.Problem0010
+{
+    class PerfectDate
+    {
+        public readonly int year;
+        public readonly int month;
+        public readonly int date;
+
+        public PerfectDate(int year, int month, int date)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+        }
+
+        public PerfectDate NextDay()
+        {
+            var newDate = date + 1;
+            var newMonth = month;
+            var newYear = year;
+            if (newDate == 33)
+            {
+                newDate = 1;
+                newMonth++;
+                if (newMonth == 9)
+                {
+                    newMonth = 1;
+                    newYear++;
+                }
+            }
+
+            return new PerfectDate(newYear, newMonth, newDate);
+        }
+    }
+}
---
+++ solutions/mohayemin/OOSolutions.CSharp/OOSolutions/Problem0010/PerfectDateTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace OOSolutions.Problem0010
+{
+    public class PerfectDateTest
+    {
+        [Fact]
+        void ARegularDay()
+        {
+            var next = new PerfectDate(1813, 1, 3).NextDay();
+            Assert.Equal(4, next.date);
+            Assert.Equal(1, next.month);
+            Assert.Equal(1813, next.year);
+        }
+
+        [Fact]
+        void LastDayOfMonth()
+        {
+            var next = new PerfectDate(1813, 1, 32).NextDay();
+            Assert.Equal(1, next.date);
+            Assert.Equal(2, next.month);
+            Assert.Equal(1813, next.year);
+        }
+
+        [Fact]
+        void LastDayOfYear()
+        {
+            var next = new PerfectDate(1813, 8, 32).NextDay();
+            Assert.Equal(1, next.date);
+            Assert.Equal(1, next.month);
+            Assert.Equal(1814, next.year);
+        }
+    }
+}
