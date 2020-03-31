49716521503b2eb38efe743215a630a2907e1f03
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-03-31 07:27:31 +0600

    add top authors by problem count widget

--- docs/src/app.ts
+++ docs/src/app.ts
@@ -2,10 +2,12 @@ import './app.scss';
 import { TopProblemsWidget } from './widgets/top-problems-widget';
 import { database } from './database';
 import { TopAuthorsByScoreWidget } from './widgets/top-authors-by-score-widget';
+import { TopAuthorsByProblemCountWidget } from './widgets/top-authors-by-problem-count-widget';
 
 const widgets = [
     new TopProblemsWidget(),
-    new TopAuthorsByScoreWidget()
+    new TopAuthorsByScoreWidget(),
+    new TopAuthorsByProblemCountWidget(),
 ];
 
 database.load().then(()=>{
---
+++ docs/src/widgets/top-authors-by-problem-count-widget.ts
@@ -0,0 +1,30 @@
+import { TableWidget } from "./table-widget";
+import { Problem } from "../models/Problem";
+import { resolveUserHtml } from '../html-resolvers/user-html-resolver';
+import { resolveProblemHtml } from '../html-resolvers/problem-html-resolver';
+import { resolveAsText } from '../html-resolvers/simple-text-html-resolver';
+import { resolveReactions } from '../html-resolvers/reactions-html-resolver';
+import { orderBy, take, sortBy, includes, groupBy, keyBy, partition, countBy, uniqBy, reduce } from "lodash";
+import { Author } from "../models/author";
+import { database } from "../database";
+
+export class TopAuthorsByProblemCountWidget extends TableWidget<Author> {
+    constructor() {
+        super('top-authors-by-problem-count',
+            'Top Authors by Problem Count',
+            [
+                { headerHtml: "Author", resolveHtml: rowData => resolveUserHtml(rowData) },
+                { headerHtml: "Total Problems", resolveHtml: rowData => resolveAsText(rowData.problems.length) },
+                { headerHtml: "Score", resolveHtml: rowData => resolveAsText(rowData.reactions.score) },
+                { headerHtml: "Total Reactions", resolveHtml: rowData => resolveReactions(rowData.reactions) }
+            ]);
+    }
+
+    loadData() {
+        let authorlist = database.authors;
+        authorlist = sortBy(authorlist, a => -a.problems.length, a => -a.reactions.score);
+        authorlist = take(authorlist, 10);
+
+        return authorlist;
+    }
+}
