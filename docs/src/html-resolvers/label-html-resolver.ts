975d881cc4f6a3d6ce62a6ee426e467c178c9a7a
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-04-01 07:07:30 +0600

    use contrast color for tet of label

--- docs/src/html-resolvers/label-html-resolver.ts
+++ docs/src/html-resolvers/label-html-resolver.ts
@@ -1,11 +1,14 @@
 import { Label } from "../models/Label";
 import * as $ from "jquery";
+import { Color } from "../utils/color";
 
-export function resolveLabelHtml(label: Label): JQuery{
+export function resolveLabelHtml(label: Label): JQuery {
+    let textColor = Color.fromHex(label.color).contrast().toRgbString();
     let $label = $("<span>")
         .addClass("label")
         .html(label.name)
         .css("background-color", `#${label.color}`)
+        .css("color", textColor)
         .attr("title", label.description);
 
     return $label;
