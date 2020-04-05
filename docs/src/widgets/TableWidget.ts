31281bebe14593fc8cc1d07c7b0d0fd84661c36b
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-04-05 09:42:05 +0600

    sorting enabled in table widget

--- docs/src/widgets/TableWidget.ts
+++ docs/src/widgets/TableWidget.ts
@@ -1,20 +1,29 @@
 import * as $ from "jquery";
 import { Widget } from "./Widget";
 import { TableColumnConfig } from "../column-configs/TableColumnConfig";
+import { orderBy, sortBy } from "lodash";
+
 
 export abstract class TableWidget<TData> extends Widget {
+    protected $table: JQuery;
+    private sortingColumn: TableColumnConfig<TData>;
+
     constructor(protected id: string, protected title: string, private columns: TableColumnConfig<TData>[]) {
         super(id, title);
     }
-    buildBodyContent() : void {
-        let $table = $("<table>")
+    buildBodyContent(): void {
+        if (this.$table)
+            this.$table.remove();
+
+        this.$table = $("<table>")
             .addClass("table table-striped table-sm table-hover");
-        let $headerRow = $("<tr>").appendTo($("<thead>").appendTo($table));
-        this.columns.forEach(col => {
-            $headerRow.append($("<th>").html(col.headerHtml));
-        });
-        let $tbody = $("<tbody>").appendTo($table);
-        let dataList = this.loadData();
+
+        this.buildTableHeader();
+
+        let $tbody = $("<tbody>").appendTo(this.$table);
+
+        let dataList = this.getSortedData();
+
         dataList.forEach((rowData, rowIndex) => {
             let $row = $("<tr>").appendTo($tbody);
             this.columns.forEach((col) => {
@@ -22,8 +31,29 @@ export abstract class TableWidget<TData> extends Widget {
                 $row.append($("<td>").append(content));
             });
         });
-        this.$body.empty();
-        this.$body.append($table);
+
+        this.$body.append(this.$table);
+    }
+    buildTableHeader() {
+        let $headerRow = $("<tr>").appendTo($("<thead>").appendTo(this.$table));
+        this.columns.forEach(col => {
+            let $th = $("<th>").html(col.headerHtml);
+            $th.click(()=> {
+                if(col.compare !== null){
+                    this.sortingColumn = col;
+                    this.buildBodyContent();
+                }
+            });
+            $headerRow.append($th);
+        });
+    }
+
+    getSortedData(){
+        let dataList = [].concat(this.loadData());
+        if(this.sortingColumn)
+            dataList.sort(this.sortingColumn.compare);
+
+        return dataList;
     }
     abstract loadData(): TData[];
 }
