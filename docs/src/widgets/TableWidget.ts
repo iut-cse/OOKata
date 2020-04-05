b6a2f339b5d54cc8f4a547a1ee5a6fbadf1bc957
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-04-05 10:19:07 +0600

    add icon to sorted column

--- docs/src/widgets/TableWidget.ts
+++ docs/src/widgets/TableWidget.ts
@@ -7,24 +7,49 @@ export abstract class TableWidget<TData> extends Widget {
     private sorting: {
         isDesc: boolean;
         column: TableColumnConfig<TData>;
+        $th: JQuery;
     }
 
     constructor(protected id: string, protected title: string, private columns: TableColumnConfig<TData>[]) {
         super(id, title);
         this.sorting = {
             isDesc: true,
-            column: { headerHtml: "", resolveHtml: null, compare: () => 0 }
+            column: { headerHtml: "", resolveHtml: null, compare: () => 0 },
+            $th: $()
         }
     }
     buildBodyContent(): void {
-        if (this.$table)
-            this.$table.remove();
-
         this.$table = $("<table>")
             .addClass("table table-striped table-sm table-hover");
 
         this.buildTableHeader();
-
+        this.buildTableBody();
+    }
+    buildTableHeader() {
+        let $headerRow = $("<tr>").appendTo($("<thead>").appendTo(this.$table));
+        this.columns.forEach(col => {
+            let $th = $("<th>").html(col.headerHtml);
+            if (col.compare) {
+                $th.addClass("sortable");
+                $th.append($("<span>").addClass("sort-icon").html(""));
+                $th.click(() => {
+                    if (this.sorting.column === col) {
+                        this.sorting.isDesc = !this.sorting.isDesc;
+                    } else {
+                        this.sorting.isDesc = false;
+                    }
+                    this.sorting.$th.removeClass("asc desc");
+                    this.sorting.$th = $th;
+                    this.sorting.column = col;
+                    this.buildTableBody();
+                    $th.addClass(this.sorting.isDesc ? "desc" : "asc");
+                });
+            }
+            $headerRow.append($th);
+        });
+    }
+    buildTableBody() {
+        this.$table.find("tbody").remove();
         let $tbody = $("<tbody>").appendTo(this.$table);
 
         let dataList = this.getSortedData();
@@ -39,33 +64,13 @@ export abstract class TableWidget<TData> extends Widget {
 
         this.$body.append(this.$table);
     }
-    buildTableHeader() {
-        let $headerRow = $("<tr>").appendTo($("<thead>").appendTo(this.$table));
-        this.columns.forEach(col => {
-            let $th = $("<th>").html(col.headerHtml);
-            if (col.compare) {
-                $th.addClass("sortable");
-                $th.click(() => {
-                    if (this.sorting.column === col) {
-                        this.sorting.isDesc = !this.sorting.isDesc;
-                    } else {
-                        this.sorting.isDesc = false;
-                    }
-                    this.sorting.column = col;
-                    this.buildBodyContent();
-                });
-            }
-            $headerRow.append($th);
-        });
-    }
-
     getSortedData() {
         let dataList = [].concat(this.loadData());
-        let comp = this.sorting.column.compare;
+        dataList = dataList.sort(this.sorting.column.compare);
         if(this.sorting.isDesc) {
-            comp = (a, b) => -this.sorting.column.compare(a, b);
+            dataList.reverse();
         }
-        dataList = dataList.sort(comp);
+        
         return dataList;
     }
     abstract loadData(): TData[];
