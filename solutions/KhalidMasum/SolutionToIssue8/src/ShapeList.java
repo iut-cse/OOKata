55c5d5ae33904d63aa18d5f581c1b75d07ee3ba9
Labnan <55809005+KhalidMasum@users.noreply.github.com>
2020-07-17 23:12:37 +0600

    Update ShapeList.java
    new public method to add shapes  instead of using strings and integers

--- solutions/KhalidMasum/SolutionToIssue8/src/ShapeList.java
+++ solutions/KhalidMasum/SolutionToIssue8/src/ShapeList.java
@@ -1,27 +1,23 @@
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ShapeList {
 
-
     private Shape[] shape;
     private double totalPerimeter = 0;
     private double totalArea = 0;
+    private int totalNumberOfShapes = 0;
 
-    public void setShapeTypeAndSize(String[] types, double[] sizes) {
-        shape = new Shape[types.length];
-        createShapeAndInsertValues(types, sizes);
+    ShapeList(int size) {
+        shape = new Shape[size];
     }
 
-    private void createShapeAndInsertValues(String[] types, double[] sizes) {
-        for (int i = 0; i < types.length; i++) {
-            createShape(types[i], sizes[i], i);
-        }
-        calculateTotalArea();
+    void addShape(Shape shape) {
+        this.shape[totalNumberOfShapes] = shape;
+        totalNumberOfShapes++;
         calculateTotalPerimeter();
+        calculateTotalArea();
     }
-
 
     public double getTotalPerimeter() {
         return totalPerimeter;
@@ -32,36 +28,28 @@ public class ShapeList {
         return totalArea;
     }
 
-
-    private void createShape(String types, double size, int index) {
-        if (types.equals("square")) {
-            shape[index] = new Square(size);
-        } else shape[index] = new Circle(size);
+    private int getLastInsertionPosition() {
+        return totalNumberOfShapes - 1;
     }
 
-
     private void calculateTotalPerimeter() {
-        for (Shape value : shape) {
-            totalPerimeter += value.getPerimeter();
-        }
+        totalPerimeter += shape[getLastInsertionPosition()].getPerimeter();
     }
 
 
     private void calculateTotalArea() {
-        for (Shape value : shape) {
-            totalArea += value.getArea();
-        }
+        totalArea += shape[getLastInsertionPosition()].getArea();
     }
 }
 
 
-// A test for ease of refactoring.
-// No need to copy this part if you are not familiar with Unit Testing.
 class ShapeListTest {
     @Test
     void testAll() {
-        ShapeList shapeList = new ShapeList();
-        shapeList.setShapeTypeAndSize(new String[]{"square", "square", "circle"}, new double[]{10, 5, 1});
+        ShapeList shapeList = new ShapeList(10);
+        shapeList.addShape(new Square(10));
+        shapeList.addShape(new Square(5));
+        shapeList.addShape(new Circle(1));
         Assertions.assertEquals(66.2831853071796, shapeList.getTotalPerimeter());
         Assertions.assertEquals(128.14159265358978, shapeList.getTotalArea());
     }
